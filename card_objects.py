ac2c97fc4e8dc2909e841ab5f08ca7e9d37ffbf1
Jeremy <jeremychristopherryan@gmail.com>
2018-05-14 18:40:32 -0400

    Added some starting classes

---
+++ card_objects.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+#   Python libraries
+import random
+
+#   Outside libraries
+
+class Card(object):
+    """   An airlock card object """
+
+    def __init__(self, game, name = 'energy'):
+        self.game = game
+        self.name = name
+
+
+class Deck(object):
+    """   A deck of airlock cards """
+
+    def __init__(self, game, expansion = True):
+        self.game = game
+        self.cards = []
+        self.populate(expansion)
+
+    def populate(self, expansion = True):
+        """ Adds the default deck. """
+
+        #   Populates the deck
+        self.add_card('energy', 13)
+        self.add_card('impact', 7)
+        self.add_card('rupture', 5)
+        self.add_card('aftershock', 3)
+        self.add_card('salvage', 2)
+        self.add_card('safeguard', 2)
+        self.add_card('hull breach', 3)
+        self.add_card('recycle', 3)
+        self.add_card('airlock', 1)
+        self.add_card('martyr', 1)
+        self.add_card('override', 3)
+        self.add_card('overrule', 3)
+        self.add_card('discharge', 3)
+        self.add_card('wormhole', 2)
+
+        #   Add expansion cards if expansion is enabled
+        if expansion:
+            self.add_card('engine failure', 2)
+            self.add_card('nullify', 3)
+            self.add_card('inflict', 2)
+            self.add_card('repair', 2)
+            self.add_card('contaminate', 2)
+            self.add_card('execute', 1)
+            self.add_card('shift', 3)
+            self.add_card('')
+
+    def add_card(self, name, num=1):
+        """ Adds 'num' cards of name 'name' to the deck. """
+
+        for i in range(0, num):
+            self.cards.append(Card(name))
+
+    def shuffle(self):
+        """ Shuffles the deck. """
+
+        random.shuffle(self.cards)
---
+++ game.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from card_objects import Card, Deck
+
+class Game(object):
+    """ Airlock game object """
+
+    def __init__(self):
+        self.create_oxygen()
+        self.deck = Deck()
+
+
+    def create_deck(self, expansion = True):
+        """ Creates the deck. """
+
+        self.deck = Deck(self, expansion)
+
+
+    def create_oxygen(self):
+        """ Creates the oxygen cells. """
+
+        self.oxygen = ['red', 'red', 'blue', 'blue', 'blue', 'blue']
+        self.force_red = False
+
+
+    def is_red_alert(self):
+        """ Returns True if the current oxygen cell is a red alert cell """
+
+        return self.oxygen[-1] == 'red' or self.force_red
+
+
+    def prompt(self, choices, prompt_string = None):
+        """ Somehow prompts the player to make a choice between items in a list.
+        This could be through text or in a GUI. """
+
+        #   TODO make this use a gui and generally be better
+
+        if prompt_string == None:
+            prompt_string == 'Choose between: '
+
+        print(prompt_string + str(choices))
+        choice = raw_input()
+
+        while choice not in choices:
+            print("That's not a valid choice. Choose again.")
+            choice = raw_input()
